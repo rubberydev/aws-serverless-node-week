91a045c892ec62840b608ef5d2f40d9fddcd197d
Julian <julian@Ingeneo-MacBook-Pro.local>
2019-08-25 22:40:58 -0500

    Workshop serverless with node js (NodeWeek by Globant)

--- handler.js
+++ handler.js
@@ -1,18 +1,38 @@
 'use strict';
 
 module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+                message: 'First exercise',
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.helloJulian = async event => {
+    const name = "Julian :)";
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-type": "text/html"
+        },
+        // body: JSON.stringify({
+        //         message: `Hello ${name}`,
+        //         input: event,
+        //     },
+        //     null,
+        //     2
+        // ),
+        body: '<html><body>I love Node</body></html>'
+    };
+
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
